ff44191feea48b470499c093341121b1bdbba64a
Daniel Garcia <danielg_97@yahoo.com>
2019-02-20 18:40:31 -0500

    Completed loadSpriteType function

--- hw1/js/wolfie/files/WolfieFileManager.js
+++ hw1/js/wolfie/files/WolfieFileManager.js
@@ -4,6 +4,31 @@ class WolfieFileManager {
     constructor() {}
 
     loadSpriteType(scene, jsonFilePath, callback) {
-        // YOU MUST DEFINE THIS
+        $.getJSON(jsonFilePath, function(data) {
+            var name = data.name;
+            var spriteSheetImage = data.spriteSheetImage;
+            var spriteWidth = data.spriteWidth;
+            var spriteHeight = data.spriteHeight;
+            var leftBuffer = data.leftBuffer;
+            var rightBuffer = data.rightBuffer;
+            var topBuffer = data.topBuffer;
+            var bottomBuffer = data.bottomBuffer;
+            var animations = data.animations;
+            var spriteTexture = scene.textures[spriteSheetImage];
+            scene.addAnimatedSpriteType(name, new AnimatedSpriteType(spriteTexture, spriteWidth, spriteHeight));
+            var spriteType = scene.getAnimatedSpriteType(name);
+            for (var i = 0; i < animations.length; i++) {
+                var state = animations[i].name;
+                var framesArray = new Array();
+                spriteType.addAnimation(state, framesArray);
+                var frames = animations[i].frames;
+                for (var j = 0; j < frames.length; j++) {
+                    var index = frames[j].index;
+                    var frameDuration = frames[j].duration;
+                    spriteType.addAnimationFrame(state, index, frameDuration);
+                }
+            }
+            callback();
+        })
     }
 }
