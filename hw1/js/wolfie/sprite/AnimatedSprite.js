8103131b87d8756a84344ebfdf050acc4904ed94
Daniel Garcia <danielg_97@yahoo.com>
2019-02-22 05:00:36 -0500

    Added next state function; Adjusted step function to account for repeat

--- hw1/js/wolfie/sprite/AnimatedSprite.js
+++ hw1/js/wolfie/sprite/AnimatedSprite.js
@@ -4,7 +4,7 @@ class AnimatedSprite {
     constructor(initSpriteType, initState) {
         this.spriteType = initSpriteType;
         this.state = initState;
-        this.animationFrameIndex = 0;
+        this.animationFrameIndex = initSpriteType.states[initState].firstFrameIndex;
         this.frameCounter = 0;
 
         // NOTE THAT THESE VALUES USE WORLD COORDINATES, WHICH WOULD CORRESPOND TO PIXEL COORDINATES
@@ -23,16 +23,27 @@ class AnimatedSprite {
         this.state = initState;
     }
 
+    moveToNextState() {
+        var nextState = this.spriteType.states[this.state].next;
+        this.state = nextState;
+        this.animationFrameIndex = this.spriteType.states[nextState].firstFrameIndex;
+        this.frameCounter = 0;
+    }
+
     step() {
         var animationState = this.spriteType.getAnimation(this.state);
         this.frameCounter++;
         if (this.frameCounter >= animationState[this.animationFrameIndex].duration) {
-            this.frameCounter = 0;
-            this.animationFrameIndex++;
-            if (this.animationFrameIndex >= animationState.length) {
-                this.animationFrameIndex = 0;
+            if (this.spriteType.states[this.state].repeat == false && 
+                this.animationFrameIndex == this.spriteType.states[this.state].lastFrameIndex) {
+                this.moveToNextState();
+            }
+            else {
+                this.frameCounter = 0;
+                this.animationFrameIndex = animationState[this.animationFrameIndex].nextFrameIndex;
             }
         }
+        
     }
 
     getLeft() {
