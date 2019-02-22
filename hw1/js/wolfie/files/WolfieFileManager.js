fab0008407ee2799e40a73570c3d3a192430638c
Daniel Garcia <danielg_97@yahoo.com>
2019-02-22 05:01:46 -0500

    Fixed/debugged code for proper state/frame changes

--- hw1/js/wolfie/files/WolfieFileManager.js
+++ hw1/js/wolfie/files/WolfieFileManager.js
@@ -20,13 +20,43 @@ class WolfieFileManager {
             for (var i = 0; i < animations.length; i++) {
                 var state = animations[i].name;
                 var framesArray = new Array();
-                spriteType.addAnimation(state, framesArray);
+                var repeat = animations[i].repeat;
+                var next = animations[i].next;
+                if (next == null) {
+                    if (i == animations.length - 1) {
+                        next = animations[0].name;
+                    }
+                    else {
+                        next = animations[i+1].name;
+                    }
+                }
+                spriteType.addAnimation(state, framesArray, i, repeat, next);
                 var frames = animations[i].frames;
                 for (var j = 0; j < frames.length; j++) {
                     var index = frames[j].index;
+                    var nextFrameIndex;
+                    if (j == frames.length - 1) {
+                        nextFrameIndex = frames[0].index;
+                    }
+                    else {
+                        nextFrameIndex = frames[j+1].index;
+                    }
                     var frameDuration = frames[j].duration;
-                    spriteType.addAnimationFrame(state, index, frameDuration);
+                    spriteType.addAnimationFrame(state, index, frameDuration, nextFrameIndex);
+                    if (j == 0) {
+                        spriteType.setFirstFrameIndex(state, index);
+                    }
+                    if (j == frames.length - 1) {
+                        spriteType.setLastFrameIndex(state, index);
+                    }
                 }
+            }
+            for (var state in spriteType.animations) {
+                var totalFrames = 0;
+                for (var frame in spriteType.animations[state]) {
+                    totalFrames++;
+                }
+                spriteType.states[state].totalFrames = totalFrames;
             }
             callback();
         })
--- hw1/js/wolfie/sprite/AnimatedSpriteType.js
+++ hw1/js/wolfie/sprite/AnimatedSpriteType.js
@@ -6,18 +6,35 @@ class AnimatedSpriteType {
         this.spriteWidth = initSpriteWidth;
         this.spriteHeight = initSpriteHeight;
         this.animations = new Array();
+        this.states = new Array();
     }
 
-    addAnimation(state, framesArray) {
+    addAnimation(state, framesArray, stateIndex, repeat, next) {
         this.animations[state] = framesArray;
+        this.states[state] = {
+            stateIndex: stateIndex,
+            repeat: repeat,
+            next: next,
+            totalFrames: 0,
+            firstFrameIndex: 0,
+            lastFrameIndex: 0
+        };
     }
 
-    addAnimationFrame(state, index, frameDuration) {
+    addAnimationFrame(state, index, frameDuration, nextFrameIndex) {
         var framesArray = this.animations[state];
-        framesArray.push({
-            index: index,
+        framesArray[index] = {
+            nextFrameIndex: nextFrameIndex,
             duration: frameDuration
-        });
+        };
+    }
+
+    setFirstFrameIndex(state, firstFrameIndex) {
+        this.states[state].firstFrameIndex = firstFrameIndex;
+    }
+
+    setLastFrameIndex(state, lastFrameIndex) {
+        this.states[state].lastFrameIndex = lastFrameIndex;
     }
 
     getAnimation(state) {
@@ -26,13 +43,14 @@ class AnimatedSpriteType {
 
     getLeft(state, frameIndex) {
         var columns = this.spriteSheetTexture.width / this.spriteWidth;
-        var left = (frameIndex % columns) * this.spriteWidth
+        var left = (frameIndex % columns) * this.spriteWidth;
         return left;
     }
 
     getTop(state, frameIndex) {
         var columns = this.spriteSheetTexture.width / this.spriteWidth;
-        var top = (Math.floor(frameIndex / columns)) * this.spriteHeight;
+        var rowIndex = Math.floor(frameIndex / columns);
+        var top = rowIndex  * this.spriteHeight;
         return top;
     }
 }
